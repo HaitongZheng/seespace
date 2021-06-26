36e66cb947c3d450a98729f00f263fe8c85b24b4
Haitong Zheng <haitongzheng32@gmail.com>
2021-06-26 13:05:28 +0800

    combine

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	var click_num = 1;
 	var $imgs = $(".fader").find("img"),i = 0;
 	function changeImg(){
 		var next = (++i % $imgs.length);
@@ -15,8 +16,7 @@ $(document).ready(function(){
 			$(".fader > img").css("height","80%");
 			$(".fader").css({"margin-left" : "20%", "margin-top":"25%"});
 			$(".fader > img").on("tap",function(){
-			$(this).hide();
-			$(".fader > img").hide();
+				$(this).hide();
 			});
 		}else{
 			$(".fader > img").css("height","100%");
@@ -24,6 +24,22 @@ $(document).ready(function(){
 			interval = setInterval(changeImg, 100);
 		};
 		$(this).data('clicks',click+1);
+		click_num++;
 	});
 
+	$(document).on('mousewheel DOMMouseScroll', function(e){
+		var wheel = e.originalEvent.wheelDelta || -e.originalEvent.detail;
+		var delta = Math.max(-1, Math.min(1, wheel) );
+		if (click_num % 2 !== 1) {
+			if(delta<0){
+				var next = (++i % $imgs.length);
+				$($imgs.get(next - 1)).fadeOut(0);
+				$($imgs.get(next)).fadeIn(0);
+			}else{
+				var next = (--i % $imgs.length);
+				$($imgs.get(next + 1)).fadeOut(0);
+				$($imgs.get(next)).fadeIn(0);
+			}
+		}
+	});
 });
