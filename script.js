231361b4505b4f242216dbb24ab29f0df2c5e5c2
Haitong Zheng <haitongzheng32@gmail.com>
2021-06-27 22:18:22 +0800

    touch

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
 	var $imgs = $(".fader").find("img"),i = 0;
 	function changeImg(){
 		var next = (++i % $imgs.length);
-		$($imgs.get(next -1)).fadeOut(100);
-		$($imgs.get(next)).fadeIn(100);
+		$($imgs.get(next -1)).fadeOut(200);
+		$($imgs.get(next)).fadeIn(200);
 	}
-	var interval = setInterval(changeImg, 100);
+	var interval = setInterval(changeImg, 500);
 
 	$("body").on('click', function(){
 		var click = +$(this).data('clicks') || 1;
@@ -21,28 +21,41 @@ $(document).ready(function(){
 		}else{
 			$(".fader > img").css("height","100%");
 			$(".fader").css("margin","0");
-			interval = setInterval(changeImg, 100);
+			interval = setInterval(changeImg, 500);
 		};
 		$(this).data('clicks',click+1);
 		click_num++;
 	});
-
+	
 	$(document).on('mousewheel DOMMouseScroll', function(e){
 		var wheel = e.originalEvent.wheelDelta || -e.originalEvent.detail;
 		var delta = Math.max(-1, Math.min(1, wheel) );
 		if (click_num % 2 !== 1) {
 			if(delta<0){
 				var next = (++i % $imgs.length);
-				$($imgs.get(next - 1)).fadeOut(0);
-				$($imgs.get(next)).fadeIn(0);
+				$($imgs.get(next - 1)).fadeOut(200);
+				$($imgs.get(next)).fadeIn(200);
 			}else{
 				var next = (--i % $imgs.length);
-				$($imgs.get(next + 1)).fadeOut(0);
-				$($imgs.get(next)).fadeIn(0);
+				$($imgs.get(next + 1)).fadeOut(200);
+				$($imgs.get(next)).fadeIn(200);
 			}
 		}
 	});
-	$(document).on('scroll', function() {
-	$(document).trigger('mousewheel');
-});
+
+	$('.fader').swipeDown((res) => {
+		if (click_num % 2 !== 1) {
+			var next = (--i % $imgs.length);
+			$($imgs.get(next + 1)).fadeOut(200);
+			$($imgs.get(next)).fadeIn(200);
+		}
+
+	})
+	$('.fader').swipeUp((res) => {
+		if (click_num % 2 !== 1) {
+			var next = (++i % $imgs.length);
+			$($imgs.get(next - 1)).fadeOut(200);
+			$($imgs.get(next)).fadeIn(200);
+		}
+	})
 });
